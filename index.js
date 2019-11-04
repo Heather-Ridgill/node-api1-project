88b497eae1b44d66a5bcc5a51cf19681af693ecc
Heather-Ridgill <Heather.Ridgill@gmail.com>
2019-11-03 19:53:54 -0800

    Finished MVP

--- index.js
+++ index.js
@@ -9,29 +9,98 @@ const server = express();
 
 
 server.listen(4000, () => {
-    console.log(`===server listening on post 4000 ===`);
+    console.log(`=== server listening on post 4000 ===`);
 });
 
+//Middleware
 server.use(express.json());
 
+//Route Handler
 
+server.get(`/`, (req, res) => {
+    res.send(`Hello World...`);
+})
+
+//GET
 server.get(`/api/users`, (req, res) => {
     db.find()
-        .then(user => {
+        .then(users => {
             res.status(200).json(users);
         })
         .catch(err => {
-            res.status(500).json({error: "The users information could not be found..."});
+            res.status(500).json({message: err, success: false
+            });
         });
 });
+ 
+//GET:ID
+server.get(`/api/users/:id` ,(req, res) => {
+const {id} = req.params;
 
-//POST Request syntax
+db.findById(id)
+    .then(user => {
+        res.status(200).json(user);
+    })
+
+    .catch(err => {
+        res.status(500).json({
+            message: err,
+            success: false
+        });
+    });
+});
+
+
+//POST 
 
 server.post(`/api/users`, (req, res) => {
     const userInfo = req.body;
+    console.log('body: ', userInfo);
 
-    db.add(userInfo)
-        .then((user) => {
-            res.status(201),json({success: true, user});
+db.insert(userInfo)
+.then((user) => {
+    res.status(201).json({success: true, user})
+})
+.catch((err) => {
+    res.status(500).json({ success: false, err});
+});
+
+});
+
+
+//PUT
+server.put(`/api/users/:id`, (req, res) =>{
+    const id = req.params.id;
+    const userInfo = req.body;
+    db.update(id, userInfo)
+        .then(user => {
+            if (user) {
+                res.status (200).json({sucess:true, user});
+            } else {
+                res.status(404).json({success: false, message: `id${id} does not exist`});
+            }
+        })
+        .catch (err => {
+            res.status(500).json({success:false, err});
         });
 });
+
+//DELETE
+
+server.delete ('/api/users/:id', (req, res) => {
+    const{id} = req.params;
+
+    db.remove(id)
+    .then(deletedUser  => {
+        if(deletedUser) {
+            res.status(204).end();
+        } else {
+            res.status(404).json({ message: `I could not find id=${id}`});
+        }
+    })
+    .catch(err => {
+        res.status(500).json({success:false, err});
+    });
+});
+
+
