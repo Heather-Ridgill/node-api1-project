34ff3b521559c393267f3977d52600bb5e23345c
Heather-Ridgill <Heather.Ridgill@gmail.com>
2019-11-03 17:57:03 -0800

    Added express and the database for index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // implement your API here
+
+
+const express = require(`express`);
+const db = require(`./data/db`);
+
+const server = express();
+
+
+
+server.listen(4000, () => {
+    console.log(`===server listening on post 4000 ===`);
+});
+
+server.use(express.json());
+
+//POST Request syntax
+
+server.post(`/api/users`, (req, res) => {
+    const userInfo = req.body;
+
+    db.add(userInfo)
+        .then((user) => {
+            res.status(201),json({success: true, user});
+        });
+});
